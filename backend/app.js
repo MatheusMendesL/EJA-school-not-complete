703e61b3c01fb0a98f4cddbf40f92e5199fa3081
MatheusMendesL <matheusmendelopes@gmail.com>
2025-08-09 00:31:58 -0300

    structure database

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,9 @@ const express = require('express')
 const app = express()
 const userRouter = require('./src/routers/user_router')
 
+app.use(cors())
 app.use(express.json())
+app.use(express.urlencoded({ extended: true}))
 
 app.get('/', (req, res) => {
     res.send('Página inicial');
---
+++ backend/src/config/database.js
@@ -0,0 +1,6 @@
+const mysql = require('mysql2')
+const db_data = require('./config_sql')
+
+const conn = mysql.createConnection(db_data)
+
+module.exports = conn
