46d8c312b5312f907bb0ed6e8df1b866f2bf620d
MatheusMendesL <matheusmendelopes@gmail.com>
2025-09-05 10:56:33 -0300

    escola continuo

--- frontend/static/js/task.js
+++ frontend/static/js/task.js
@@ -1,4 +1,4 @@
-async function task(){
+async function task() {
 
     const token = localStorage.getItem("token");
     const urlParams = window.location.search;
@@ -14,22 +14,75 @@ async function task(){
 
     const tasks = await fetch('task/lesson/' + id, {
         method: 'GET',
-        headers:{
-             "Content-Type": "application/json",
-              Authorization: `Bearer ${token}`,
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
         }
     }).then(response => {
         if (response.status == 200) return response.json();
         throw new Error("Erro para devolver os dados");
-    }).then(data => {return data})
+    }).then(data => { return data })
 
 
     // ver retorno
     console.log(tasks)
 
-    // passar p os lugares da task.html
+    let index = 0
+    let percentage = 10
 
-    // pegar id da materia
+    function showQuestion() {
+        const q = tasks.data[index];
+
+        document.querySelector("#question").innerText = q.question;
+        document.querySelector("#optionA").innerText = q.option_a;
+        document.querySelector("#optionB").innerText = q.option_b;
+        document.querySelector("#optionC").innerText = q.option_c;
+        document.querySelector("#optionD").innerText = q.option_d;
+        document.querySelector("#number").textContent = index + 1
+        document.querySelector('.progress-fill').style.width = `${percentage}%`;
+    }
+
+
+    let correto = 0
+    let errado = 0
+
+
+    function checkAnswer(answer) {
+        const q = tasks.data[index];
+
+        // na escola, passar aq oq é correto e errado
+
+        document.querySelector('.btn_submit').addEventListener('click', (e) => {
+            e.preventDefault()
+            if (answer === q.correct) {
+                // fetch p api de acerto
+            } else {
+                // fetch p api de erro
+            }
+
+            index++;
+            percentage += 10
+            if (index < tasks.data.length) {
+
+                // pede p acalmar, primeira mente ele tem q mostrar se está correto
+                showQuestion();
+            } else {
+                // passar p pagina de xp
+            }
+        })
+
+
+    }
+
+
+
+
+    document.querySelector("#optionA").addEventListener("click", () => checkAnswer("option_a"));
+    document.querySelector("#optionB").addEventListener("click", () => checkAnswer("option_b"));
+    document.querySelector("#optionC").addEventListener("click", () => checkAnswer("option_c"));
+    document.querySelector("#optionD").addEventListener("click", () => checkAnswer("option_d"));
+
+    showQuestion()
 
 }
 
